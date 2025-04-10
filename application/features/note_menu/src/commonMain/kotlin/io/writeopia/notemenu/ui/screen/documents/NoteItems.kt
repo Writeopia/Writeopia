a2447f41e962e86d6c547fdba8d9b7bc7fe3efa4
Leandro Ferreira <email@email.com>
2025-04-10 19:48:17 +0200

    Update NoteItems.kt

--- application/features/note_menu/src/commonMain/kotlin/io/writeopia/notemenu/ui/screen/documents/NoteItems.kt
+++ application/features/note_menu/src/commonMain/kotlin/io/writeopia/notemenu/ui/screen/documents/NoteItems.kt
@@ -658,7 +658,7 @@ private fun DocumentItem(
                     }
 
                     Row(modifier = Modifier.align(Alignment.TopEnd).height(40.dp).padding(12.dp)) {
-                        if (!documentUi.isFavorite) {
+                        if (documentUi.isFavorite) {
                             Icon(
                                 imageVector = WrIcons.favorites,
                                 contentDescription = "Favorite",
