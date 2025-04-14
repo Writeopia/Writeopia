63913fa57f06aa8ec5459c604e66f2270b700bf2
Leandro Ferreira <email@email.com>
2025-04-14 18:06:50 +0200

    Update KmpFileReader.js.kt

--- plugins/writeopia_import_document/src/jsMain/kotlin/io/writeopia/sdk/import/files/KmpFileReader.js.kt
+++ plugins/writeopia_import_document/src/jsMain/kotlin/io/writeopia/sdk/import/files/KmpFileReader.js.kt
@@ -9,15 +9,16 @@ actual object KmpFileReader {
     actual inline fun <reified T> readObjects(filePaths: List<String>, json: Json): Flow<T> =
         flow { }
 
-    actual inline fun <reified T> readDirectory(
-        directoryPath: String,
-        json: Json
-    ): Flow<T> = flow { }
-
     actual inline fun <reified T> readObject(
         filePath: String,
         json: Json
     ): T? {
         TODO("Not yet implemented")
     }
+
+    actual inline fun <reified T> readDirectory(
+        directoryPath: String,
+        json: Json,
+        crossinline predicate: (String) -> Boolean
+    ): Flow<T> = flow {  }
 }
