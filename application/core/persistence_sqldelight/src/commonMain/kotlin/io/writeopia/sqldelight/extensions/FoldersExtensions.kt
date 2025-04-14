811bc8fee1af8978bda2e30182b75727ad3ba283
Leandro Borges Ferreira <lehen01@gmail.com>
2025-04-14 04:29:56 +0200

    Changing folder icon (#405)
    * Update FoldersExtensions.kt
    
    * Update HeaderDrawer.kt
    
    ---------
    
    Co-authored-by: Leandro Ferreira <email@email.com>

--- application/core/persistence_sqldelight/src/commonMain/kotlin/io/writeopia/sqldelight/extensions/FoldersExtensions.kt
+++ application/core/persistence_sqldelight/src/commonMain/kotlin/io/writeopia/sqldelight/extensions/FoldersExtensions.kt
@@ -3,6 +3,7 @@ package io.writeopia.sqldelight.extensions
 import io.writeopia.app.sql.FolderEntity
 import io.writeopia.common.utils.extensions.toBoolean
 import io.writeopia.models.Folder
+import io.writeopia.sdk.models.document.MenuItem
 import kotlinx.datetime.Instant
 
 fun FolderEntity.toModel(count: Long) =
@@ -15,4 +16,8 @@ fun FolderEntity.toModel(count: Long) =
         userId = user_id,
         itemCount = count,
         favorite = favorite.toBoolean(),
+        icon = if (this.icon != null && this.icon_tint != null) MenuItem.Icon(
+            this.icon,
+            this.icon_tint.toInt()
+        ) else null
     )
