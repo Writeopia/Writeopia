ddefa16a1f67e99f36604e1afcdc7afe08976813
Leandro Ferreira <email@email.com>
2025-05-17 18:53:07 +0200

    Fixing android crash

--- application/core/auth_core/src/androidMain/kotlin/io/writeopia/auth/core/di/AuthCoreInjectionNeo.android.kt
+++ application/core/auth_core/src/androidMain/kotlin/io/writeopia/auth/core/di/AuthCoreInjectionNeo.android.kt
@@ -1,34 +1,33 @@
 package io.writeopia.auth.core.di
 
-import android.content.SharedPreferences
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import io.writeopia.auth.core.data.AuthApi
 import io.writeopia.auth.core.manager.AuthRepository
-import io.writeopia.common.utils.di.SharedPreferencesInjector
+import io.writeopia.auth.core.manager.SqlDelightRepository
+import io.writeopia.di.AppConnectionInjection
+import io.writeopia.sdk.network.injector.WriteopiaConnectionInjector
+import io.writeopia.sql.WriteopiaDb
+import io.writeopia.sqldelight.di.WriteopiaDbInjector
 
-actual class AuthCoreInjectionNeo private constructor(
-    private val sharedPreferences: SharedPreferences
+actual class AuthCoreInjectionNeo(
+    private val writeopiaDb: WriteopiaDb? = WriteopiaDbInjector.singleton()?.database,
+    private val appConnectionInjection: AppConnectionInjection = AppConnectionInjection.singleton(),
+    private val connectionInjector: WriteopiaConnectionInjector =
+        WriteopiaConnectionInjector.singleton()
 ) {
 
-    private val auth: FirebaseAuth = Firebase.auth
+    actual fun provideAuthRepository(): AuthRepository = SqlDelightRepository(writeopiaDb)
 
-    actual fun provideAuthApi(): AuthApi {
-        TODO("Not yet implemented")
-    }
-
-    actual fun provideAuthRepository(): AuthRepository {
-        TODO("Not yet implemented")
-    }
+    actual fun provideAuthApi(): AuthApi =
+        AuthApi(
+            client = appConnectionInjection.provideHttpClient(),
+            baseUrl = connectionInjector.baseUrl()
+        )
 
     actual companion object {
         private var instance: AuthCoreInjectionNeo? = null
 
         actual fun singleton(): AuthCoreInjectionNeo =
-            instance ?: AuthCoreInjectionNeo(
-                SharedPreferencesInjector.singleton().sharedPreferences
-            ).also {
+            instance ?: AuthCoreInjectionNeo().also {
                 instance = it
             }
     }
