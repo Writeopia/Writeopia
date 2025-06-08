307321120c33a957dc2629eee537c154b5efb1fd
Leandro Ferreira <email@email.com>
2025-06-08 23:24:47 +0200

    Update MainDesktop.kt

--- application/composeApp/src/jvmMain/kotlin/io/writeopia/desktop/MainDesktop.kt
+++ application/composeApp/src/jvmMain/kotlin/io/writeopia/desktop/MainDesktop.kt
@@ -3,7 +3,6 @@ package io.writeopia.desktop
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
