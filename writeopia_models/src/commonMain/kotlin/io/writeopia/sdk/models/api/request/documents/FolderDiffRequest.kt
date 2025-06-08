654fa537e8206d278c73d3ab02225eff843ec78a
Leandro Ferreira <email@email.com>
2025-06-08 12:36:47 +0200

    Update FolderDiffRequest.kt

--- writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/api/request/documents/FolderDiffRequest.kt
+++ writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/api/request/documents/FolderDiffRequest.kt
@@ -3,4 +3,4 @@ package io.writeopia.sdk.models.api.request.documents
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class FolderDiffRequest(val folderId: String, val userId: String, val lastFolderSync: Long)
+data class FolderDiffRequest(val folderId: String, val lastFolderSync: Long)
