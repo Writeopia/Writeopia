89ad38a6a520a5d029d0ffddb15edaa216c1e9cb
Leandro Ferreira <email@email.com>
2025-06-08 10:54:02 +0200

    format

--- backend/documents/documents/src/main/java/io/writeopia/api/documents/documents/dto/SendDocumentsRequest.kt
+++ backend/documents/documents/src/main/java/io/writeopia/api/documents/documents/dto/SendDocumentsRequest.kt
@@ -4,5 +4,4 @@ import io.writeopia.sdk.serialization.data.DocumentApi
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class SendDocumentsRequest(val documents: List<DocumentApi>
-)
+data class SendDocumentsRequest(val documents: List<DocumentApi>)
