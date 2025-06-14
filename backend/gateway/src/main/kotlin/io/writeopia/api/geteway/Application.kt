5c958deb178ff0303f6c0f20e85e1e7af13f3a40
Leandro Ferreira <email@email.com>
2025-06-14 18:37:38 +0200

    adding test docker-compose

--- backend/gateway/src/main/kotlin/io/writeopia/api/geteway/Application.kt
+++ backend/gateway/src/main/kotlin/io/writeopia/api/geteway/Application.kt
@@ -11,7 +11,8 @@ fun main() {
     embeddedServer(
         Netty,
         port = 8080,
-        host = "127.0.0.1",
+//        host = "127.0.0.1",
+        host = "0.0.0.0",
         module = Application::module
     ).start(wait = true)
 }
