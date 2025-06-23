b1eb29e241a18e1e0d2beaac24353198e263cef0
Leandro Ferreira <email@email.com>
2025-06-23 10:32:03 +0200

    code clean

--- application/core/documents/src/jvmMain/kotlin/io/writeopia/core/folders/di/FoldersInjector.jvm.kt
+++ application/core/documents/src/jvmMain/kotlin/io/writeopia/core/folders/di/FoldersInjector.jvm.kt
@@ -1,7 +1,7 @@
 package io.writeopia.core.folders.di
 
-import io.writeopia.core.folders.repository.FolderRepository
-import io.writeopia.core.folders.repository.FolderRepositorySqlDelight
+import io.writeopia.core.folders.repository.folder.FolderRepository
+import io.writeopia.core.folders.repository.folder.FolderRepositorySqlDelight
 import io.writeopia.sql.WriteopiaDb
 import io.writeopia.sqldelight.dao.FolderSqlDelightDao
 import io.writeopia.sqldelight.di.WriteopiaDbInjector
