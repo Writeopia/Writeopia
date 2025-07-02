9786aeac5f4feb212cde09af7c210dc18a0dd82d
Leandro Ferreira <email@email.com>
2025-07-02 18:17:11 +0200

    fixing library

--- application/core/payments/src/commonMain/kotlin/io/writeopia/core/payments/ui/Paywall.kt
+++ application/core/payments/src/commonMain/kotlin/io/writeopia/core/payments/ui/Paywall.kt
@@ -2,14 +2,15 @@ package io.writeopia.core.payments.ui
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import com.revenuecat.purchases.kmp.ui.revenuecatui.Paywall
+import com.revenuecat.purchases.kmp.ui.revenuecatui.PaywallOptions
 
 @Composable
 fun ProOfferScreen() {
     val options = remember {
-        
-//        PaywallOptions(dismissRequest = { TODO("Handle dismiss") }) {
-//            shouldDisplayDismissButton = true
-//        }
+        PaywallOptions(dismissRequest = { }) {
+            shouldDisplayDismissButton = true
+        }
     }
 
     Paywall(options)
