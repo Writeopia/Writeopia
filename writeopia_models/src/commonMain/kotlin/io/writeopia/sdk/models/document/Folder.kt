84f0eb3ef0a23123ca7ab732ec5831be6fbe4a70
Leandro Ferreira <email@email.com>
2025-07-08 18:29:14 +0200

    Registering and adding test

--- writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/document/Folder.kt
+++ writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/document/Folder.kt
@@ -10,7 +10,7 @@ data class Folder(
     override val title: String,
     override val createdAt: Instant,
     override val lastUpdatedAt: Instant,
-    override val userId: String,
+    override val workspaceId: String,
     override val favorite: Boolean = false,
     override val icon: MenuItem.Icon? = null,
     val itemCount: Long,
@@ -30,7 +30,7 @@ data class Folder(
                 createdAt = now,
                 lastUpdatedAt = now,
                 itemCount = 0,
-                userId = userId,
+                workspaceId = userId,
             )
         }
     }
