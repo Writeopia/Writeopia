281bd348f621d612cc0518de41035c70f8cbeb19
Leandro Ferreira <email@email.com>
2025-07-08 20:54:16 +0200

    fixing implementation

--- plugins/writeopia_serialization/src/commonMain/kotlin/io/writeopia/sdk/serialization/data/auth/AuthResponse.kt
+++ plugins/writeopia_serialization/src/commonMain/kotlin/io/writeopia/sdk/serialization/data/auth/AuthResponse.kt
@@ -1,6 +1,5 @@
 package io.writeopia.sdk.serialization.data.auth
 
-import io.writeopia.sdk.models.Workspace
 import io.writeopia.sdk.serialization.data.WorkspaceApi
 import io.writeopia.sdk.serialization.data.WriteopiaUserApi
 import kotlinx.serialization.Serializable
