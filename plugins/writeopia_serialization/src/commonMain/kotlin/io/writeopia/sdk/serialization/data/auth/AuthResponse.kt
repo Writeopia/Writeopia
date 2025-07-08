37d543de19b9f23a4daa1a706603dc1f67eb8b7d
Leandro Ferreira <email@email.com>
2025-07-08 18:01:44 +0200

    Creating workspace api

--- plugins/writeopia_serialization/src/commonMain/kotlin/io/writeopia/sdk/serialization/data/auth/AuthResponse.kt
+++ plugins/writeopia_serialization/src/commonMain/kotlin/io/writeopia/sdk/serialization/data/auth/AuthResponse.kt
@@ -1,7 +1,13 @@
 package io.writeopia.sdk.serialization.data.auth
 
+import io.writeopia.sdk.models.Workspace
+import io.writeopia.sdk.serialization.data.WorkspaceApi
 import io.writeopia.sdk.serialization.data.WriteopiaUserApi
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AuthResponse(val token: String?, val writeopiaUser: WriteopiaUserApi)
+data class AuthResponse(
+    val token: String?,
+    val writeopiaUser: WriteopiaUserApi,
+    val workspace: WorkspaceApi
+)
