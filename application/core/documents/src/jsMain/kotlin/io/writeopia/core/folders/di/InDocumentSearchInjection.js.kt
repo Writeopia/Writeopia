9c7c21a45960928aa714b6514cec7beba7747162
Leandro Ferreira <email@email.com>
2025-08-28 15:04:28 +0200

    fixing web

--- application/core/documents/src/jsMain/kotlin/io/writeopia/core/folders/di/InDocumentSearchInjection.js.kt
+++ application/core/documents/src/jsMain/kotlin/io/writeopia/core/folders/di/InDocumentSearchInjection.js.kt
@@ -1,15 +1,28 @@
 package io.writeopia.core.folders.di
 
-import io.writeopia.core.folders.repository.InDocumentSearchSqlRepository
+import io.writeopia.core.folders.repository.InDocumentSearchRepository
+import io.writeopia.sdk.models.story.StoryStep
 
 actual class InDocumentSearchInjection {
-    actual fun provideInDocumentSearchRepo(): InDocumentSearchSqlRepository {
-        TODO("Not yet implemented")
+    actual fun provideInDocumentSearchRepo(): InDocumentSearchRepository {
+        return object : InDocumentSearchRepository {
+            override fun searchInDocument(
+                query: String,
+                documentId: String
+            ): Set<Int> = emptySet()
+
+            override suspend fun insertForFts(
+                storyStep: StoryStep,
+                documentId: String,
+                position: Int
+            ) {}
+        }
     }
 
     actual companion object {
-        actual fun singleton(): InDocumentSearchInjection {
-            TODO("Not yet implemented")
-        }
+        private var instance: InDocumentSearchInjection? = null
+
+        actual fun singleton(): InDocumentSearchInjection =
+            instance ?: InDocumentSearchInjection()
     }
 }
