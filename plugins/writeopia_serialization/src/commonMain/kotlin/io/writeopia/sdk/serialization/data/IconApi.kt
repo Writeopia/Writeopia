744e704b8dd3c7aaaba8074e620aedb65c41de71
Leandro Ferreira <email@email.com>
2025-09-01 19:01:58 +0200

    fixing build

--- plugins/writeopia_serialization/src/commonMain/kotlin/io/writeopia/sdk/serialization/data/IconApi.kt
+++ plugins/writeopia_serialization/src/commonMain/kotlin/io/writeopia/sdk/serialization/data/IconApi.kt
@@ -3,4 +3,4 @@ package io.writeopia.sdk.serialization.data
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class IconApi(val label: String, val tint: Long?)
+data class IconApi(val label: String, val tint: Int?)
