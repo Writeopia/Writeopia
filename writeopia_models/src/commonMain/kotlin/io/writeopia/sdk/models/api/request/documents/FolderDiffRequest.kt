ba43587dca516063d13989f0ec90ccce7d188c18
Leandro Ferreira <email@email.com>
2025-09-01 15:24:01 +0200

    creating endpoint for diff of workspace

--- writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/api/request/documents/FolderDiffRequest.kt
+++ writeopia_models/src/commonMain/kotlin/io/writeopia/sdk/models/api/request/documents/FolderDiffRequest.kt
@@ -3,4 +3,8 @@ package io.writeopia.sdk.models.api.request.documents
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class FolderDiffRequest(val folderId: String, val lastFolderSync: Long)
+data class FolderDiffRequest(
+    val folderId: String,
+    val workspaceId: String,
+    val lastFolderSync: Long
+)
