277a2429ab01858f520f8b1e9604573adb476bb9
Leandro Borges Ferreira <lehen01@gmail.com>
2025-12-12 16:36:13 +0100

    Versions bump (#548)

--- application/core/auth_core/src/androidMain/kotlin/io/writeopia/auth/core/token/AppBearerTokenHandler.android.kt
+++ application/core/auth_core/src/androidMain/kotlin/io/writeopia/auth/core/token/AppBearerTokenHandler.android.kt
@@ -1,14 +1,7 @@
 package io.writeopia.auth.core.token
 
-import android.util.Log
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.ktx.Firebase
 import io.writeopia.sdk.network.oauth.BearerTokenHandler
-import kotlinx.coroutines.tasks.await
 
 actual object AppBearerTokenHandler : BearerTokenHandler {
-    actual override suspend fun getIdToken(): String? =
-        Firebase.auth.currentUser?.getIdToken(true)?.await()?.token?.also { token ->
-            Log.d("FirebaseTokenHandler", "token: $token")
-        }
+    actual override suspend fun getIdToken(): String? = null
 }
