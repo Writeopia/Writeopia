85c28f7f00f045ff444e8387d47360a7c8dcb360
Leandro Borges Ferreira <lehen01@gmail.com>
2026-06-04 12:51:37 -0300

    baseline

--- application/common_flows/wide_screen_common/src/commonMain/kotlin/io/writeopia/notes/desktop/components/DesktopApp.kt
+++ application/common_flows/wide_screen_common/src/commonMain/kotlin/io/writeopia/notes/desktop/components/DesktopApp.kt
@@ -61,7 +61,6 @@ import io.writeopia.ui.draganddrop.target.DraggableScreen
 import io.writeopia.ui.keyboard.KeyboardEvent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
