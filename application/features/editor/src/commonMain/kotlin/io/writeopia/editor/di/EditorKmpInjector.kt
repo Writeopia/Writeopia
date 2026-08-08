d67359c91e8febb0067159289effcb026dee1157
Leandro Borges Ferreira <lehen01@gmail.com>
2026-08-08 18:09:26 -0300

    Fixing text selection2 (#714)
    * Fixing text selection
    
    * ktlint

--- application/features/editor/src/commonMain/kotlin/io/writeopia/editor/di/EditorKmpInjector.kt
+++ application/features/editor/src/commonMain/kotlin/io/writeopia/editor/di/EditorKmpInjector.kt
@@ -64,6 +64,7 @@ class EditorKmpInjector private constructor(
     private val workspaceInjection: WorkspaceInjection =
         WorkspaceInjection.singleton(),
     private val imageUploader: ImageUploader? = null,
+    val textSelectionActiveState: MutableStateFlow<Boolean> = MutableStateFlow(false),
 ) : TextEditorInjector {
 
     // SharedFlow for drawing save events - ViewModel subscribes to this
@@ -87,7 +88,8 @@ class EditorKmpInjector private constructor(
         keyboardEventFlow = keyboardEventFlow,
         documentRepository = repositoryInjection.provideDocumentRepository(),
         userRepository = authRepository,
-        imageUploader = imageUploader
+        imageUploader = imageUploader,
+        textSelectionActiveState = textSelectionActiveState
     )
 
     private fun provideNoteEditorViewModel(
