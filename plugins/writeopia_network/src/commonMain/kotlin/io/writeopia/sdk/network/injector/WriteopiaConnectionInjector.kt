7f78ec9e2d531e7b35fd674e87140bc0029fc04b
Leandro Borges Ferreira <lehen01@gmail.com>
2026-08-26 17:02:01 -0300

    import fix

--- plugins/writeopia_network/src/commonMain/kotlin/io/writeopia/sdk/network/injector/WriteopiaConnectionInjector.kt
+++ plugins/writeopia_network/src/commonMain/kotlin/io/writeopia/sdk/network/injector/WriteopiaConnectionInjector.kt
@@ -2,7 +2,6 @@ package io.writeopia.sdk.network.injector
 
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.plugins.logging.DEFAULT
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
