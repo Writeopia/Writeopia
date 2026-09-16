f05712d4ed2961adf961bac138a6a2b8240a82ea
glutoide <glutoide1@gmail.com>
2026-09-16 22:59:41 +0300

    refactor(desktop): move update feature into module

---
+++ application/features/update/build.gradle.kts
@@ -0,0 +1,45 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    kotlin("multiplatform")
+    alias(libs.plugins.compose.multiplatform)
+    alias(libs.plugins.compose.multiplatform.compiler)
+    alias(libs.plugins.ktlint)
+}
+
+kotlin {
+    jvmToolchain(21)
+
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_21)
+        }
+    }
+
+    sourceSets {
+        val jvmMain by getting {
+            dependencies {
+                implementation(project(":application:core:configuration"))
+                implementation(project(":application:core:resources"))
+                implementation(project(":plugins:writeopia_network"))
+
+                implementation(compose.runtime)
+                implementation(compose.foundation)
+                implementation(compose.material3)
+                implementation(compose.ui)
+
+                implementation(libs.ktor.client.core)
+            }
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+                implementation(libs.kotlinx.coroutines.test)
+                implementation(libs.ktor.client.mock)
+                implementation(libs.ktor.client.content.negotiation)
+                implementation(libs.ktor.serialization.json)
+            }
+        }
+    }
+}
