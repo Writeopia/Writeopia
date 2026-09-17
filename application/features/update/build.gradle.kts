719c48ffe69cb117e1c8aaac794979e4a6fe0089
glutoide <glutoide1@gmail.com>
2026-09-17 21:55:22 +0300

    refactor(update): add lifecycle viewmodel support

--- application/features/update/build.gradle.kts
+++ application/features/update/build.gradle.kts
@@ -30,6 +30,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.material3)
                 implementation(compose.ui)
+                implementation(libs.lifecycle.viewmodel.compose)
 
                 implementation(libs.ktor.client.core)
                 implementation(libs.kotlinx.serialization.json)
