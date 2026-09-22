4ad879d91e20c5d8e03c41f627e4df0f842b8ab7
Leandro Borges Ferreira <lehen01@gmail.com>
2026-09-22 17:32:47 -0300

    Update AccountDeletionStartedScreen.kt

--- application/features/account/src/commonMain/kotlin/io/writeopia/account/ui/AccountDeletionStartedScreen.kt
+++ application/features/account/src/commonMain/kotlin/io/writeopia/account/ui/AccountDeletionStartedScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -35,7 +36,7 @@ fun AccountDeletionStartedScreen(
             imageVector = WrIcons.check,
             contentDescription = null,
             tint = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.height(48.dp)
+            modifier = Modifier.size(64.dp)
         )
 
         Spacer(modifier = Modifier.height(16.dp))
