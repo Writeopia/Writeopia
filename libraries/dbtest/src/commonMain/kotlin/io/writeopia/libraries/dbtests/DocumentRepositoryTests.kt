60c730459de0a744c583dd7faeb5f01d3a35ec39
glutoide <glutoide1@gmail.com>
2026-09-22 11:08:29 +0300

    Test parent-load comment preservation

--- libraries/dbtest/src/commonMain/kotlin/io/writeopia/libraries/dbtests/DocumentRepositoryTests.kt
+++ libraries/dbtest/src/commonMain/kotlin/io/writeopia/libraries/dbtests/DocumentRepositoryTests.kt
@@ -242,6 +242,15 @@ class DocumentRepositoryTests(private val documentRepository: DocumentRepository
         assertEquals(document.commentConversations, loadedDocument.commentConversations)
     }
 
+    suspend fun parentLoadPreservesComments() {
+        val document = saveDocumentWithComments()
+        val loadedDocument =
+            documentRepository.loadDocumentsByParentId(document.parentId, document.workspaceId)
+                .first { it.id == document.id }
+
+        assertEquals(document.commentConversations, loadedDocument.commentConversations)
+    }
+
     suspend fun commentPersistenceRespectsWorkspaceBoundaries() {
         val now = now()
 
