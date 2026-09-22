dafd694a05ead8acfc62c48333103224e57c1d6d
Leandro Borges Ferreira <lehen01@gmail.com>
2026-09-22 13:30:29 -0300

    Update AccountDeletionServiceTest.kt

--- backend/core/auth/src/test/kotlin/io/writeopia/api/core/auth/service/AccountDeletionServiceTest.kt
+++ backend/core/auth/src/test/kotlin/io/writeopia/api/core/auth/service/AccountDeletionServiceTest.kt
@@ -36,6 +36,7 @@ class AccountDeletionServiceTest {
         db.insertUser(
             id = userId,
             name = "Test User",
+            username = "user_name",
             email = userEmail,
             password = "hashedpassword",
             salt = "salt",
