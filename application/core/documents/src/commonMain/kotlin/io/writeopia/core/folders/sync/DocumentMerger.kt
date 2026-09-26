daaabea860e3e2b31736e7f5a1d6534933473c7b
glutoide <glutoide1@gmail.com>
2026-09-26 15:05:25 +0300

    Fix document merger formatting

--- application/core/documents/src/commonMain/kotlin/io/writeopia/core/folders/sync/DocumentMerger.kt
+++ application/core/documents/src/commonMain/kotlin/io/writeopia/core/folders/sync/DocumentMerger.kt
@@ -83,6 +83,7 @@ class DocumentMerger {
             yieldAll(child.referencedCommentConversationIds())
         }
     }
+
     private fun mergeContent(
         localContent: Map<Double, StoryStep>,
         backendContent: Map<Double, StoryStep>
